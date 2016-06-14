8af9e646b5461c87703732d607da041a65f6d608
Mark <zhouyao0808@gmail.com>
2016-06-14 14:45:48 -0700

    almost final

---
+++ mlclas/ensemble/__init__.py
@@ -0,0 +1,7 @@
+from mlclas.ensemble.ensembles import BinaryRelevance, ClassifierChains, CalibratedLabelRanking, RandomKLabelsets, MLKNN
+
+__all__ = ['BinaryRelevance',
+           'ClassifierChains',
+           'CalibratedLabelRanking',
+           'RandomKLabelsets',
+           'MLKNN']
